2325ddb1c547fb003f9f0791a995b544f11f14b8
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2022-06-17 09:18:32 +0200

    New Version

--- assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const menuBtn = document.querySelector(".menu .icon");
-const menuLink = document.querySelector(".menu .menu__link");
-menuBtn.addEventListener("click", () => {
-	menuBtn.classList.toggle("open");
-	menuLink.classList.toggle("active");
-});
